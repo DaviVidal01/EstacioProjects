49f57efdc36829938f30135dadf49934bfb30596
Davi Vidal <davividal0605@gmail.com>
2025-06-03 06:10:22 -0300

    Finalizando o Nível Aventureiro

---
+++ Introdução a Programação/SuperTrunfo em C/2 - Nivel Aventureiro/Solucao Estruturada/exemplo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Função Genérica para entrada de dados
+void entradaDados(){
+    // código para a função entradaDados
+}
+
+//Função genérica para cálculo da média
+float calcularMedia(){
+    // código para a função calcularMedia
+}
+
+//Função genérica para exibir dados
+void exibirMedia(){
+    //Codigo para a função exibirMedia
+}
+
+int main()
+{
+    entradaDados();
+    float media = calcularMedia();
+    exibirMedia();
+    return 0;
+}
